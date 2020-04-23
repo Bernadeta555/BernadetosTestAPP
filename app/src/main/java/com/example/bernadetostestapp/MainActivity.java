e1dfb1d0db9b0c6f6218ad787efaa80220f16b0b
bernadeta <bernadetalukaseviciute@gmail.com>
2020-04-23 16:52:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bernadetostestapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.bernadetostestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onbuttonMainClick(View view) {
+        TextView textViewMain = findViewById(R.id.textviewMain);
+        textViewMain.setText(R.string.new_text);
+    }
+}
